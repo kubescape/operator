a27d06ee4502fe0b3901bc27859f346f30c34dda
David Wertenteil <dwertent@cyberarmor.io>
2020-01-06 18:38:41 +0200

    inject wlid

--- websocket/updateworkload.go
+++ websocket/updateworkload.go
@@ -19,43 +19,43 @@ func updateWorkload(workload interface{}, wlid string) error {
 	switch kind {
 	case "Deployment":
 		w := workload.(*appsv1.Deployment)
-		injectWlid(w.Spec.Template.ObjectMeta.Annotations, wlid)
+		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
 		go clientset.AppsV1().Deployments(namespace).Update(w)
 
 	case "ReplicaSet":
 		w := workload.(*appsv1.ReplicaSet)
-		injectWlid(w.Spec.Template.ObjectMeta.Annotations, wlid)
+		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
 		go clientset.AppsV1().ReplicaSets(namespace).Update(w)
 
 	case "DaemonSet":
 		w := workload.(*appsv1.DaemonSet)
-		injectWlid(w.Spec.Template.ObjectMeta.Annotations, wlid)
+		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
 		go clientset.AppsV1().DaemonSets(namespace).Update(w)
 
 	case "StatefulSet":
 		w := workload.(*appsv1.StatefulSet)
-		injectWlid(w.Spec.Template.ObjectMeta.Annotations, wlid)
+		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
 		go clientset.AppsV1().StatefulSets(namespace).Update(w)
 
 	case "PodTemplate":
 		w := workload.(*corev1.PodTemplate)
-		injectWlid(w.ObjectMeta.Annotations, wlid)
+		injectWlid(&w.ObjectMeta.Annotations, wlid)
 		go clientset.CoreV1().PodTemplates(namespace).Update(w)
 
 	case "Pod":
 		w := workload.(*corev1.Pod)
-		injectWlid(w.ObjectMeta.Annotations, wlid)
+		injectWlid(&w.ObjectMeta.Annotations, wlid)
 		go clientset.CoreV1().Pods(namespace).Update(w)
 	}
 	return nil
 
 }
 
-func injectWlid(annotations map[string]string, wlid string) {
-	if annotations == nil {
-		annotations = make(map[string]string)
+func injectWlid(annotations *map[string]string, wlid string) {
+	if *annotations == nil {
+		(*annotations) = make(map[string]string)
 
 	}
-	annotations["wlid"] = wlid
-	annotations["latets-catriger-update"] = string(time.Now().UTC().Format("02-01-2006 15:04:05"))
+	(*annotations)["wlid"] = wlid
+	(*annotations)["latets-catriger-update"] = string(time.Now().UTC().Format("02-01-2006 15:04:05"))
 }
