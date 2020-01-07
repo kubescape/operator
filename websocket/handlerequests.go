c3c47be880115b3b80de46386978034ba290c3fa
David Wertenteil <dwertent@cyberarmor.io>
2020-01-07 10:53:04 +0200

    update labels

--- websocket/handlerequests.go
+++ websocket/handlerequests.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"encoding/json"
+	"fmt"
 	"k8s-ca-websocket/cautils"
 	"k8s-ca-websocket/sign"
 
@@ -23,23 +24,28 @@ var (
 
 // HandlePostmanRequest Parse received commands and run the command
 func (wsh *WebSocketHandler) HandlePostmanRequest(receivedCommands []byte) []error {
-	// log.Printf("Recveived: %v", string(receivedCommands))
 	commands := cautils.Commands{}
 	errorList := []error{}
 
 	if err := json.Unmarshal(receivedCommands, &commands); err != nil {
 		glog.Error(err)
-		glog.Infoln("Failed CyberArmor websocket")
 		return []error{err}
 	}
 	for _, c := range commands.Commands {
+		if c.Wlid == "" || c.CommandName == "" {
+			err := fmt.Errorf("command or wlid not found. wlid: %s, command: %s", c.Wlid, c.CommandName)
+			glog.Error(err)
+			return []error{err}
+		}
 		go func(c cautils.Command) {
+			status := "SUCCESS"
 			glog.Infof("Running %s command, wlid: %s", c.CommandName, c.Wlid)
 			if err := wsh.runCommand(c); err != nil {
 				glog.Errorf("%v", err)
+				status = "FAIL"
 				errorList = append(errorList, err)
 			}
-			glog.Infof("Done %s command, wlid: %s", c.CommandName, c.Wlid)
+			glog.Infof("Done command %s, wlid: %s, status: %s", c.CommandName, c.Wlid, status)
 		}(c)
 	}
 	return errorList
@@ -53,9 +59,9 @@ func (wsh *WebSocketHandler) runCommand(c cautils.Command) error {
 
 	switch c.CommandName {
 	case UPDATE:
-		return updateWorkload(workload, c.Wlid)
+		return updateWorkload(workload, c.Wlid, UPDATE)
 	case REMOVE:
-		return removeWorkload(workload, c.Wlid)
+		return updateWorkload(workload, c.Wlid, REMOVE)
 	case SIGN:
 		return signWorkload(workload, c.Wlid)
 
@@ -72,5 +78,5 @@ func signWorkload(workload interface{}, wlid string) error {
 		return err
 	}
 	glog.Infof("Done signing, updating workload, wlid: %s", wlid)
-	return updateWorkload(workload, wlid)
+	return updateWorkload(workload, wlid, SIGN)
 }
--- websocket/updateworkload.go
+++ websocket/updateworkload.go
@@ -1,15 +1,17 @@
 package websocket
 
 import (
+	"k8s-ca-webhook/cautils"
 	"k8s-ca-websocket/k8sworkloads"
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-func updateWorkload(workload interface{}, wlid string) error {
+func updateWorkload(workload interface{}, wlid string, command string) error {
 	microservice, _ := RestoreMicroserviceIDsFromSpiffe(wlid)
 	namespace, kind := microservice[1], microservice[2]
 	clientset, err := kubernetes.NewForConfig(k8sworkloads.GetK8sConfig())
@@ -19,43 +21,77 @@ func updateWorkload(workload interface{}, wlid string) error {
 	switch kind {
 	case "Deployment":
 		w := workload.(*appsv1.Deployment)
-		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
+		inject(&w.Spec.Template.ObjectMeta, command, wlid)
 		go clientset.AppsV1().Deployments(namespace).Update(w)
 
 	case "ReplicaSet":
 		w := workload.(*appsv1.ReplicaSet)
-		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
+		inject(&w.Spec.Template.ObjectMeta, command, wlid)
 		go clientset.AppsV1().ReplicaSets(namespace).Update(w)
 
 	case "DaemonSet":
 		w := workload.(*appsv1.DaemonSet)
-		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
+		inject(&w.Spec.Template.ObjectMeta, command, wlid)
 		go clientset.AppsV1().DaemonSets(namespace).Update(w)
 
 	case "StatefulSet":
 		w := workload.(*appsv1.StatefulSet)
-		injectWlid(&w.Spec.Template.ObjectMeta.Annotations, wlid)
+		inject(&w.Spec.Template.ObjectMeta, command, wlid)
 		go clientset.AppsV1().StatefulSets(namespace).Update(w)
 
 	case "PodTemplate":
 		w := workload.(*corev1.PodTemplate)
-		injectWlid(&w.ObjectMeta.Annotations, wlid)
+		inject(&w.ObjectMeta, command, wlid)
 		go clientset.CoreV1().PodTemplates(namespace).Update(w)
 
 	case "Pod":
 		w := workload.(*corev1.Pod)
-		injectWlid(&w.ObjectMeta.Annotations, wlid)
+		inject(&w.ObjectMeta, command, wlid)
 		go clientset.CoreV1().Pods(namespace).Update(w)
 	}
 	return nil
 
 }
 
+func inject(metadata *v1.ObjectMeta, command, wlid string) {
+	switch command {
+	case UPDATE:
+		injectWlid(&metadata.Annotations, wlid)
+		injectTime(&metadata.Annotations)
+
+	case SIGN:
+		updateLabel(&metadata.Labels)
+		injectTime(&metadata.Annotations)
+
+	case REMOVE:
+		removeCAMetadata(metadata)
+	}
+
+}
+
 func injectWlid(annotations *map[string]string, wlid string) {
 	if *annotations == nil {
 		(*annotations) = make(map[string]string)
-
 	}
 	(*annotations)["wlid"] = wlid
+}
+
+func injectTime(annotations *map[string]string) {
+	if *annotations == nil {
+		(*annotations) = make(map[string]string)
+	}
 	(*annotations)["latets-catriger-update"] = string(time.Now().UTC().Format("02-01-2006 15:04:05"))
+}
+
+func updateLabel(labels *map[string]string) {
+	if *labels == nil {
+		(*labels) = make(map[string]string)
+	}
+	(*labels)[cautils.CALabel] = "signed"
+}
+
+func removeCAMetadata(meatdata *v1.ObjectMeta) {
+	delete(meatdata.Labels, CAInject)
+	delete(meatdata.Labels, CALabel)
+	delete(meatdata.Annotations, "wlid")
 }
