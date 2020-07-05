aa8f1b5fcce57b13aee99a5aa94ca83e927d580a
David Wertenteil <dwertent@cyberarmor.io>
2020-07-05 11:53:46 +0300

    typo

--- websocket/cawebsocket_test.go
+++ websocket/cawebsocket_test.go
@@ -5,6 +5,37 @@ import (
 	"testing"
 )
 
+// //wss://postman.eudev2.cyberarmorsoft.com/waitfornotification/5d817063-096f-4d91-b39b-8665240080af-dav-att-test1
+// func CreateWebSocketHandlerMock() *WebSocketHandler {
+// 	var websocketURL WebSocketURL
+// 	websocketURL.Scheme = "wss"
+// 	websocketURL.Host = "postman.eudev2.cyberarmorsoft.com"
+// 	websocketURL.Path = fmt.Sprintf("waitfornotification/5d817063-096f-4d91-b39b-8665240080af-dav-att-test1")
+// 	websocketURL.ForceQuery = false
+// 	return &WebSocketHandler{
+// 		data:         make(chan DataSocket),
+// 		webSocketURL: websocketURL,
+// 	}
+
+// }
+
+//wss://postman.onprem.eudev2.cyberarmorsoft.com/waitfornotification/1e3a88bf-92ce-44f8-914e-cbe71830d566-onprem-test-1
+func CreateWebSocketHandlerMock() *WebSocketHandler {
+	var websocketURL WebSocketURL
+	websocketURL.Scheme = "wss"
+	websocketURL.Host = "postman.onprem.eudev3.cyberarmorsoft.com"
+	websocketURL.Path = fmt.Sprintf("waitfornotification/1e3a88bf-92ce-44f8-914e-cbe71830d566-onprem-test-1")
+	websocketURL.ForceQuery = false
+	return &WebSocketHandler{
+		data:         make(chan DataSocket),
+		webSocketURL: websocketURL,
+	}
+
+}
 func TestDoNothoing1(t *testing.T) {
-	fmt.Printf("Do Nothing")
+	// wsh := CreateWebSocketHandlerMock()
+	// cautils.CA_IGNORE_VERIFY_CACLI = true
+	// if _, err := wsh.dialWebSocket(); err != nil {
+	// 	t.Errorf("%v", err)
+	// }
 }
