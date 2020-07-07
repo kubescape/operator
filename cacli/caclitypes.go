d3fc26459b66319a33106aa1c3e6222f75aed5ce
David Wertenteil <dwertent@cyberarmor.io>
2020-07-07 15:43:41 +0300

    casignerr

--- cacli/caclitypes.go
+++ cacli/caclitypes.go
@@ -3,7 +3,7 @@ package cacli
 // Status cacli status
 type Status struct {
 	CacliVersion     string `json:"cacli-version"`
-	CacsignerVersion string `json:"cacsigner-version"`
+	CacsignerVersion string `json:"casigner-version"`
 	Server           string `json:"server"`
 	Customer         string `json:"customer"`
 	UserName         string `json:"user-name"`
