d9f4dd57e5ce4a19d9ef1df6ab33d368fd6aeff9
David Wertenteil <dwertent@cyberarmor.io>
2020-08-12 14:57:22 +0300

    logs

--- websocket/getworkload.go
+++ websocket/getworkload.go
@@ -31,7 +31,7 @@ func getWorkloadFromK8S(namespace, kind, name string) (interface{}, error) {
 				return clientset.AppsV1().Deployments(namespace).Get(i.Name, v1.GetOptions{})
 			}
 		}
-		return nil, fmt.Errorf("Deployment %s not found", name)
+		return nil, fmt.Errorf("Deployment '%s' not found in namespace: %s", name, namespace)
 
 	case "ReplicaSet":
 		w, _ := clientset.AppsV1().ReplicaSets(namespace).List(v1.ListOptions{})
@@ -40,7 +40,7 @@ func getWorkloadFromK8S(namespace, kind, name string) (interface{}, error) {
 				return clientset.AppsV1().ReplicaSets(namespace).Get(i.Name, v1.GetOptions{})
 			}
 		}
-		return nil, fmt.Errorf("ReplicaSet %s not found", name)
+		return nil, fmt.Errorf("ReplicaSet '%s' not found in namespace: %s", name, namespace)
 	case "DaemonSet":
 		w, _ := clientset.AppsV1().DaemonSets(namespace).List(v1.ListOptions{})
 		for _, i := range w.Items {
@@ -48,7 +48,7 @@ func getWorkloadFromK8S(namespace, kind, name string) (interface{}, error) {
 				return clientset.AppsV1().DaemonSets(namespace).Get(i.Name, v1.GetOptions{})
 			}
 		}
-		return nil, fmt.Errorf("DaemonSet %s not found", name)
+		return nil, fmt.Errorf("DaemonSet '%s' not found in namespace: %s", name, namespace)
 	case "StatefulSet":
 		w, _ := clientset.AppsV1().StatefulSets(namespace).List(v1.ListOptions{})
 		for _, i := range w.Items {
@@ -56,7 +56,7 @@ func getWorkloadFromK8S(namespace, kind, name string) (interface{}, error) {
 				return clientset.AppsV1().StatefulSets(namespace).Get(i.Name, v1.GetOptions{})
 			}
 		}
-		return nil, fmt.Errorf("StatefulSet %s not found", name)
+		return nil, fmt.Errorf("StatefulSet '%s' not found in namespace: %s", name, namespace)
 	case "PodTemplate":
 		w, _ := clientset.CoreV1().PodTemplates(namespace).List(v1.ListOptions{})
 		for _, i := range w.Items {
@@ -64,7 +64,7 @@ func getWorkloadFromK8S(namespace, kind, name string) (interface{}, error) {
 				return clientset.CoreV1().PodTemplates(namespace).Get(i.Name, v1.GetOptions{})
 			}
 		}
-		return nil, fmt.Errorf("PodTemplate %s not found", name)
+		return nil, fmt.Errorf("PodTemplate '%s' not found in namespace: %s", name, namespace)
 	case "Pod":
 		w, _ := clientset.CoreV1().Pods(namespace).List(v1.ListOptions{})
 		for _, i := range w.Items {
@@ -72,7 +72,7 @@ func getWorkloadFromK8S(namespace, kind, name string) (interface{}, error) {
 				return clientset.CoreV1().Pods(namespace).Get(i.Name, v1.GetOptions{})
 			}
 		}
-		return nil, fmt.Errorf("Pod %s not found", name)
+		return nil, fmt.Errorf("Pod '%s' not found in namespace: %s", name, namespace)
 	}
 	return nil, fmt.Errorf("kind: %s unknown", kind)
 
