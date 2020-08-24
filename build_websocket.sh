c245555bd40a81d744ec2cc2b3e6a87970bfa7b0
David Wertenteil <dwertent@cyberarmor.io>
2020-08-24 11:11:58 +0300

    update test build

--- build_websocket.sh
+++ build_websocket.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 set -ex
 
+export ITAG=latest
+export WTAG=test
+
+# dep ensure
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o k8s-ca-websocket .
 chmod +x k8s-ca-websocket
-docker build --no-cache -f Dockerfile.test -t dreg.eust0.cyberarmorsoft.com:443/k8s-ca-websocket:test .
 
+# # base image
+# docker build --no-cache -f Dockerfile.base -t dreg.eust0.cyberarmorsoft.com:443/k8s-base-image:$ITAG .
+# docker push dreg.eust0.cyberarmorsoft.com:443/k8s-base-image:$ITAG
+
+docker build --no-cache -f Dockerfile.test -t dreg.eust0.cyberarmorsoft.com:443/k8s-ca-websocket:$WTAG .
 rm -rf k8s-ca-websocket
+# docker push dreg.eust0.cyberarmorsoft.com:443/k8s-ca-websocket:$WTAG
+ 
 
-# docker push dreg.eust0.cyberarmorsoft.com:443/k8s-ca-websocket
+kubectl -n cyberarmor-system patch  deployment ca-websocket -p '{"spec": {"template": {"spec": { "containers": [{"name": "ca-websocket", "imagePullPolicy": "Never"}]}}}}' || true
+kubectl -n cyberarmor-system set image deployment/ca-websocket ca-websocket=dreg.eust0.cyberarmorsoft.com:443/k8s-ca-websocket:$WTAG || true
+kubectl delete pod -n cyberarmor-system $(kubectl get pod -n cyberarmor-system | grep websocket |  awk '{print $1}')
+kubectl logs -f -n cyberarmor-system $(kubectl get pod -n cyberarmor-system | grep websocket |  awk '{print $1}')
