9d4cebc6b86357604152b3c80ce5a043d16ed33f
david <dwertent@cyberarmor.io>
2021-03-30 17:08:58 +0300

    fixed build file

--- build_websocket.sh
+++ build_websocket.sh
@@ -7,17 +7,13 @@ export WTAG=test
 # dep ensure
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o k8s-ca-websocket .
 chmod +x k8s-ca-websocket
-
-# base image
-# docker build --no-cache -f Dockerfile.base -t dreg.eust0.cyberarmorsoft.com:443/k8s-base-image:$ITAG .
-# docker push dreg.eust0.cyberarmorsoft.com:443/k8s-base-image:$ITAG
-
+ 
 docker build --no-cache -f Dockerfile.test -t quay.io/armosec/k8s-ca-websocket-ubi:$WTAG .
 rm -rf k8s-ca-websocket
-# docker push quay.io/armosec/k8s-ca-websocket:$WTAG
+# docker push quay.io/armosec/k8s-ca-websocket-ubi:$WTAG
  
 
-kubectl -n cyberarmor-system patch  deployment ca-websocket -p '{"spec": {"template": {"spec": { "containers": [{"name": "ca-websocket-container", "imagePullPolicy": "Never"}]}}}}' || true
-kubectl -n cyberarmor-system set image deployment/ca-websocket ca-websocket-container=quay.io/armosec/k8s-ca-websocket-ubi:$WTAG || true
+kubectl -n cyberarmor-system patch  deployment ca-websocket -p '{"spec": {"template": {"spec": { "containers": [{"name": "ca-websocket", "imagePullPolicy": "Never"}]}}}}' || true
+kubectl -n cyberarmor-system set image deployment/ca-websocket ca-websocket=quay.io/armosec/k8s-ca-websocket-ubi:$WTAG || true
 kubectl delete pod -n cyberarmor-system $(kubectl get pod -n cyberarmor-system | grep websocket |  awk '{print $1}')
 kubectl logs -f -n cyberarmor-system $(kubectl get pod -n cyberarmor-system | grep websocket |  awk '{print $1}')
