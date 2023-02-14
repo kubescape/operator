4e362becce2bbe4344e24bb7376eb201d03beeda
Daniel-GrunbergerCA <danielg@armosec.io>
2023-02-14 09:40:10 +0200

    call handleWatchers
    Signed-off-by: Daniel-GrunbergerCA <danielg@armosec.io>

--- main.go
+++ main.go
@@ -70,6 +70,7 @@ func main() {
 
 	isReadinessReady = true
 
+	go mainHandler.HandleWatchers(ctx)
 	// wait for requests to come from the websocket or from the REST API
 	mainHandler.HandleRequest(ctx)
 
@@ -83,5 +84,4 @@ func initHttpHandlers() {
 	mainhandler.KubescapeHttpClient = utils.InitKubescapeHttpClient()
 	mainhandler.VulnScanHttpClient = utils.InitVulnScanHttpClient()
 	utils.ReporterHttpClient = utils.InitReporterHttpClient()
-
 }
