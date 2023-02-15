aca265c488101af89842d6916ef2cd4675ad9703
Daniel-GrunbergerCA <danielg@armosec.io>
2023-02-15 11:06:27 +0200

    rebase

--- mainhandler/vulnscan.go
+++ mainhandler/vulnscan.go
@@ -559,7 +559,11 @@ func (actionHandler *ActionHandler) sendCommandForContainers(ctx context.Context
 		}
 	}
 
-	return fmt.Errorf(errs)
+	if errs != "" {
+		return fmt.Errorf(errs)
+	}
+
+	return nil
 }
 
 func sendCommandToScanner(ctx context.Context, webSocketScanCommand *apis.WebsocketScanCommand, command apis.NotificationPolicyType) error {
