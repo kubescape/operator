cf6d1211d980196cf0d4155f2768a2c81d5a3710
idohu <idoh@armosec.io>
2024-06-27 15:28:17 +0300

    modify tests
    Signed-off-by: idohu <idoh@armosec.io>

--- servicehandler/servicediscovery_test.go
+++ servicehandler/servicediscovery_test.go
@@ -78,6 +78,63 @@ func TestInitialPorts(t *testing.T) {
 	}
 }
 
+// func TestDiscoveryServiceHandler(t *testing.T) {
+
+// 	ctx := context.Background()
+
+// 	kubeconfig := os.Getenv("HOME") + "/.kube/config" // Adjust path as necessary
+
+// 	// Build kubeconfig from file
+// 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+
+// 	// Create Kubernetes clientset
+// 	clientset, err := kubernetes.NewForConfig(config)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+
+// 	kubeClient := clientset.CoreV1()
+// 	dynamicClient, _ := dynamic.NewForConfig(config)
+// 	dynamicClient1 := dynamicClient.Resource(Schema)
+// 	logger.L().Ctx(ctx).Info(kubeconfig)
+
+// 	logger.L().Ctx(ctx).Info("starting a new service discovery handling")
+// 	currentServiceList := make(currentServiceList, 0)
+
+// 	// get a list of all  services in the cluster
+// 	services, err := kubeClient.Services("").List(context.TODO(), serviceListOptions)
+// 	t.Log(services, err)
+// 	if err != nil {
+// 		logger.L().Ctx(ctx).Error(err.Error())
+// 		return
+// 	}
+
+// 	scansWg := sync.WaitGroup{}
+// 	antsPool, _ := ants.NewPool(20)
+
+// 	for _, service := range services.Items {
+// 		logger.L().Ctx(ctx).Info(fmt.Sprint(service.Name, service.Namespace))
+// 		sra := ServiceAuthentication{}
+// 		sra.kind = kind
+// 		sra.apiVersion = apiVersion
+// 		sra.metadata.name = service.Name
+// 		sra.metadata.namespace = service.Namespace
+// 		sra.spec.clusterIP = service.Spec.ClusterIP
+// 		sra.initialPorts(service.Spec.Ports)
+
+// 		currentServiceList = append(currentServiceList, [2]string{service.Name, service.Namespace})
+// 		sra.Discover(ctx, &scansWg, antsPool, dynamicClient1)
+// 	}
+// 	scansWg.Wait()
+// 	antsPool.Release()
+
+// 	currentServiceList.deleteServices(ctx, dynamicClient1)
+// 	logger.L().Ctx(ctx).Info("finished service discovery cycle")
+// }
+
 //TODO: Add more tests
 
 //TODO use fake client
