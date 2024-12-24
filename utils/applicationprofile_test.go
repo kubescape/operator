28a27f5af0749785294daa8532719440e3942cd4
Amir Malka <amirm@armosec.io>
2024-12-24 15:22:16 +0200

    trigger AP scan in kubevuln whenever the application profile can be used (#277)
    * trigger AP scan in kubevuln whenever the application profile can be used
    
    Signed-off-by: Amir Malka <amirm@armosec.io>
    
    * fix tests
    
    Signed-off-by: Amir Malka <amirm@armosec.io>
    
    ---------
    
    Signed-off-by: Amir Malka <amirm@armosec.io>

---
+++ utils/applicationprofile_test.go
@@ -0,0 +1,87 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+
+	helpersv1 "github.com/kubescape/k8s-interface/instanceidhandler/v1/helpers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSkipApplicationProfile(t *testing.T) {
+	tests := []struct {
+		annotations map[string]string
+		name        string
+		wantSkip    bool
+		expectedErr error
+	}{
+		{
+			name: "status is empty",
+			annotations: map[string]string{
+				helpersv1.StatusMetadataKey:     "",
+				helpersv1.WlidMetadataKey:       "wlid",
+				helpersv1.InstanceIDMetadataKey: "instanceID",
+			},
+			wantSkip: false,
+		},
+		{
+			name: "status is Ready",
+			annotations: map[string]string{
+				helpersv1.StatusMetadataKey:     helpersv1.Ready,
+				helpersv1.WlidMetadataKey:       "wlid",
+				helpersv1.InstanceIDMetadataKey: "instanceID",
+			},
+			wantSkip: false,
+		},
+		{
+			name: "status is Completed",
+			annotations: map[string]string{
+				helpersv1.StatusMetadataKey:     helpersv1.Completed,
+				helpersv1.WlidMetadataKey:       "wlid",
+				helpersv1.InstanceIDMetadataKey: "instanceID",
+			},
+			wantSkip: false,
+		},
+		{
+			name: "status is not recognized",
+			annotations: map[string]string{
+				helpersv1.StatusMetadataKey: "NotRecognized",
+			},
+			wantSkip:    true,
+			expectedErr: fmt.Errorf("invalid status"),
+		},
+		{
+			name:        "no status annotation",
+			annotations: map[string]string{},
+			wantSkip:    true,
+			expectedErr: fmt.Errorf("no annotations"),
+		},
+		{
+			name: "missing instance WLID annotation",
+			annotations: map[string]string{
+				helpersv1.StatusMetadataKey:     helpersv1.Ready,
+				helpersv1.InstanceIDMetadataKey: "instanceID",
+			},
+			wantSkip:    true,
+			expectedErr: fmt.Errorf("missing WLID annotation"),
+		},
+
+		{
+			name: "missing instance ID annotation",
+			annotations: map[string]string{
+				helpersv1.StatusMetadataKey: helpersv1.Ready,
+				helpersv1.WlidMetadataKey:   "wlid",
+			},
+			wantSkip:    true,
+			expectedErr: fmt.Errorf("missing InstanceID annotation"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotSkip, err := SkipApplicationProfile(tt.annotations)
+			assert.Equal(t, tt.wantSkip, gotSkip)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
